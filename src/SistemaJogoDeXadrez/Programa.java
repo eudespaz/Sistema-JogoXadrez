0ca64a3d8df5470121a027831d1f381ca4c3a102
Eudes Paz <eudespaz@live.com>
2019-07-31 16:44:55 -0300

    Class Posição

--- src/SistemaJogoDeXadrez/Programa.java
+++ src/SistemaJogoDeXadrez/Programa.java
@@ -1,11 +1,15 @@
 package SistemaJogoDeXadrez;
 
+import gameborda.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Ola");
-		System.out.println("Eudes Paz");
-		System.out.println("Paz");
+	
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
+	
 
 	}
 
